0f90a4d643a3c5b8d116c2e40d2f024be4ff189f
obedWambugu <obedwambugu12@gmail.com>
2024-09-04 12:08:51 +0300

    Delete book-network directory

--- book-network/src/main/java/com/wambuguObed/book_network/exception/ActivationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.wambuguObed.book_network.exception;
-
-public class ActivationTokenException extends RuntimeException {
-    public ActivationTokenException(String message) {
-        super(message);
-    }
-}
